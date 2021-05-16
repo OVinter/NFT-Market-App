4290ecef67567e97faa2fcb2d9f2072639c60d24
OVinter <ovidiu.vinter99@gmail.com>
2021-05-16 18:50:52 +0300

    refactoringLogin/SignUp

--- NFTApp/src/app/app-routing.module.ts
+++ NFTApp/src/app/app-routing.module.ts
@@ -3,10 +3,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DummyComponent} from './dummy/dummy.component';
 import {SignupComponent} from './signup/signup.component';
-import {LoginComponent} from './login/login.component';
 import {MyNftsComponent} from './my-nfts/my-nfts.component';
 import {AddNftComponent} from './add-nft/add-nft.component';
-import {ModifyNftComponent} from "./modify-nft/modify-nft.component";
+import {ModifyNftComponent} from './modify-nft/modify-nft.component';
 
 
 const routes: Routes = [
@@ -15,8 +14,7 @@ const routes: Routes = [
   { path: 'nfts/modify', component: ModifyNftComponent },
   { path: 'nfts/add', component: AddNftComponent },
   { path: 'nfts', component: MyNftsComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
--- NFTApp/src/app/app.module.ts
+++ NFTApp/src/app/app.module.ts
@@ -7,7 +7,6 @@ import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
 import { DummyComponent } from './dummy/dummy.component';
 import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
 import {FormsModule} from '@angular/forms';
 import { MyNftsComponent } from './my-nfts/my-nfts.component';
 import { AddNftComponent } from './add-nft/add-nft.component';
@@ -20,7 +19,6 @@ import { ModifyNftComponent } from './modify-nft/modify-nft.component';
     NavbarComponent,
     DummyComponent,
     SignupComponent,
-    LoginComponent,
     MyNftsComponent,
     AddNftComponent,
     ModifyNftComponent
